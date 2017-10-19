6e0faac216a02b233f80a9056570eca1213ce405
Wells <chawkeye@gmail.com>
2017-10-19 10:56:02 +0800

    Edit and Delete Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,6 +25,12 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    @post.destroy
+
+    redirect_to root_path
+  end
+
   private
   def post_params
     params.require(:post).permit(:title, :body)
