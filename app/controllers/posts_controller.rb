a99df8a004b6f4974e7dc6cf24ba00ba6604c2b9
Wells <chawkeye@gmail.com>
2017-10-19 09:47:39 +0800

    Add Posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,27 @@
+class PostsController < ApplicationController
+  before_action :set_params, :only => [:show, :edit, :update, :destroy]
+  def index
+    @posts = Post.all.order("created_at DESC")
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render "new"
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+  def set_params
+    @post = Post.find(params[:id])
+  end
+end
